1e3e18fe3da4516f9eff4edcd181f16db605df3e
david tuan <super496@hotmail.com>
2016-04-01 19:27:48 +0000

    added var playground

---
+++ variable-playground.js
@@ -0,0 +1,25 @@
+// var person = {
+//     name : "david",
+//     age: 21
+// };
+
+// function updatePerson(obj){
+//     obj = {
+//         name : 'david t',
+//         age:18
+//     };
+//     obj.age = 24;
+// }
+// updatePerson(person);
+// console.log(person.age);
+
+//array example
+
+var ary = [15,27];
+function addArray(ary){
+    // ary = [15,28,34];
+    ary.push(29);
+    debugger;
+}
+addArray(ary);
+console.log(ary);

c4c6c05449e67c9cf01832c35f6bf2a169789fd9
david tuan <super496@hotmail.com>
2016-04-07 00:16:30 +0000

    Added auth middleware and findbytoken class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require("body-parser");
 var _ = require("underscore");
 var db = require("./db.js");
 var bcrypt = require("bcrypt");
+var middleware = require("./middleware.js")(db);
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -16,7 +17,7 @@ app.get('/', function(req, res) {
 });
 
 // GET /todos?completed=true&q=house
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
     var query = req.query;
     var where = {};
 
@@ -46,7 +47,7 @@ app.get('/todos', function(req, res) {
 });
 
 // GET /todos/:id
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoID = parseInt(req.params.id, 10);
 
     db.sequelize.sync().then(function() {
@@ -65,7 +66,7 @@ app.get('/todos/:id', function(req, res) {
 
 
 // POST /todos
-app.post('/todos', function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
     // validation
@@ -89,7 +90,7 @@ app.post('/todos', function(req, res) {
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
     // var body = req.body;
     var where = {};
     var todoID = parseInt(req.params.id, 10);
@@ -110,7 +111,7 @@ app.delete('/todos/:id', function(req, res) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
     var todoID = parseInt(req.params.id, 10);
     var body = _.pick(req.body, 'description', 'completed');
     var attributes = {};

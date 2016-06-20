d1ba7b2d86339f1f592d7f5320cfa5fa70021e20
Taiwei Tuan <taiweituan@gmail.com>
2016-06-20 17:50:00 -0500

    Added Log in page

--- public/js/Todos.js
+++ public/js/Todos.js
@@ -4,10 +4,19 @@
 
 ])
 
-//.config(['$mdThemingProvider', '$routeProvider', function ($mdThemingProvider, $routeProvider) {
- 
-//}])
+.config(['$routeProvider', function ($r) {
+    // ng-route
+    $r.when('/', {
+        title: "Todos Home",
+        templateUrl: 'pages/home.html',
+        reloadOnSearch: false,
+    });
+}])
 
 .controller('mainController', ['$scope', function ($s) {
-    $s.test = "Hello World!";
+    $s.mainTest = "Hello Main World!";
+}])
+
+.controller('homeController', ['$scope', function ($s) {
+    $s.test = "Hello Home World!";
 }]);
